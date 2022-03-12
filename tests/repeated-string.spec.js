48f1d620e9a0d1982e7870fd5f783e86780ff9f3
Conner Gesbocker <gesbocker@gmail.com>
2022-03-12 10:52:55 -0700

    Feature branch: work in progress

--- tests/repeated-string.spec.js
+++ tests/repeated-string.spec.js
@@ -1,14 +1,16 @@
 const repeatedString = require("../repeated-string");
 
 describe("repeated-string.js", () => {
-  test("Should be 588525: ", () => expect(repeatedString("aab", 882787)).toEqual(588525);
+  test("Should be 588525: ", () =>
+    expect(repeatedString("aab", 882787)).toEqual(588525));
 
-  console.log(
-    "Should be 51574523448: ",
-    repeatedString(
-      "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm",
-      736778906400
-    )
-  );
-  console.log("Should be 2: ", repeatedString("ababa", 3));
+  test("Should be 51574523448: ", () =>
+    expect(
+      repeatedString(
+        "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm",
+        736778906400
+      )
+    ).toEqual(51574523448));
+
+  test("Should be 2: ", () => expect(repeatedString("ababa", 3)).toEqual(2));
 });
