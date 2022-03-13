32ff58578318ed1572fbccf5e8ba3f81de9195e8
Conner Gesbocker <gesbocker@gmail.com>
2022-03-13 08:39:40 -0600

    Feature branch: work in progress

--- minimum-swaps.js
+++ minimum-swaps.js
@@ -1,4 +1,4 @@
-console.log = () => null;
+// console.log = () => null;
 
 function swap(array, indexOne, indexTwo) {
   let value = array[indexOne];
@@ -58,19 +58,31 @@ function minimumSwapsNoMap(arr) {
     }
 
     // 4
-    let currentValue = arr[i - 1];
-
-    // [ , true ]
+    let currentIndexValue = arr[i - 1];
+    console.log("currentIndexValue", currentIndexValue);
     isVisited[i] = true;
 
-    if (node === i) {
+    if (currentIndexValue === i) {
+      // 1 -> in first spot
       continue;
     } else {
-      while (!isVisited[currentValue]) {
-        isVisited[currentValue] = true;
-        let nextValue = arr[currentValue];
-        arr = swap(arr, currentValue - 1, nextValue - 1);
-        currentValue = nextValue;
+      // while current value hasn't been visited
+      // swap it with the value at the index - 1 of
+      // the value in this position
+      while (!isVisited[currentIndexValue]) {
+        // store
+        isVisited[currentIndexValue] = true;
+
+        // get value at index
+        let valueAtIndex = arr[currentIndexValue - 1];
+        console.log("value at index", valueAtIndex);
+
+        arr = swap(arr, i - 1, currentIndexValue - 1);
+        console.log("swapped", arr);
+        currentIndexValue = valueAtIndex;
+
+        console.log("setting current index value", currentIndexValue);
+
         minimumSwaps++;
       }
     }
@@ -79,14 +91,14 @@ function minimumSwapsNoMap(arr) {
   return minimumSwaps;
 }
 
-console.info("Should be 3", minimumSwaps([4, 3, 1, 2]));
-console.info("Should be 3", minimumSwaps([2, 3, 4, 1, 5]));
-console.info("Should be 3", minimumSwaps([1, 3, 5, 2, 4, 6, 7]));
-console.info("Should be 1", minimumSwaps([1, 4, 3, 2]));
-console.info("Should be 9", minimumSwaps([3, 7, 6, 9, 1, 8, 10, 4, 2, 5]));
+// console.info("Should be 3", minimumSwapsNoMap([4, 3, 1, 2]));
+console.info("Should be 3", minimumSwapsNoMap([2, 3, 4, 1, 5]));
+console.info("Should be 3", minimumSwapsNoMap([1, 3, 5, 2, 4, 6, 7]));
+console.info("Should be 1", minimumSwapsNoMap([1, 4, 3, 2]));
+console.info("Should be 9", minimumSwapsNoMap([3, 7, 6, 9, 1, 8, 10, 4, 2, 5]));
 console.info(
   "Should be 46",
-  minimumSwaps([
+  minimumSwapsNoMap([
     2, 31, 1, 38, 29, 5, 44, 6, 12, 18, 39, 9, 48, 49, 13, 11, 7, 27, 14, 33,
     50, 21, 46, 23, 15, 26, 8, 47, 40, 3, 32, 22, 34, 42, 16, 41, 24, 10, 4, 28,
     36, 30, 37, 35, 20, 17, 45, 43, 25, 19,
