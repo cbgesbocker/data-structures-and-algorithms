a4363781e99ff61f096697e31eeb3020c0074445
Conner Gesbocker <gesbocker@gmail.com>
2022-03-17 09:18:44 -0600

    Feature branch: work in progress

--- minimum-costs.js
+++ minimum-costs.js
@@ -5,15 +5,6 @@ function getSum(arr) {
   }, 0);
 }
 
-function incrementMapValue(map, key) {
-  let val = map.get(key);
-  if (val) {
-    map.set(key, val + 1);
-  } else {
-    map.set(key, 1);
-  }
-  return map;
-}
 // Complete the getMinimumCost function below.
 function getMinimumCost(k, c) {
   if (k === c.length) {
@@ -23,27 +14,27 @@ function getMinimumCost(k, c) {
   c.sort((a, b) => b - a);
 
   let numUsers = k;
-  let userNumPurchasesMap = new Map();
+  let usersList = [];
 
   for (let i = 0; i < c.length; i++) {
     // for each person and allowed purchase
     // purchase the most expensive flower
     while (numUsers > 0 && i < c.length) {
-      userNumPurchasesMap = incrementMapValue(userNumPurchasesMap, i);
+      usersList[i] = i;
       numUsers--;
       i++;
     }
 
     // all users have purchased the most expensive flowers first
     // now we need to purchase the rest in reverse order of the map
-    let end = userNumPurchasesMap.size - 1;
+    let end = usersList.length - 1;
     let currentPurchaseNumber = 1;
     while (end >= 0 && i < c.length) {
       c[i] = c[i] * (1 + currentPurchaseNumber);
       end--;
       i++;
       if (end < 0 && i < c.length) {
-        end = userNumPurchasesMap.size - 1;
+        end = usersList.length - 1;
         currentPurchaseNumber++;
         continue;
       }
@@ -51,6 +42,3 @@ function getMinimumCost(k, c) {
   }
   return getSum(c);
 }
-
-console.log("Should be 15", getMinimumCost(2, [2, 5, 6]));
-console.log("Should be 29", getMinimumCost(3, [1, 3, 5, 7, 9]));
