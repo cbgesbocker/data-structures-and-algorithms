2b42971640db7b8194c800e2854e3fa78fabf9ac
Conner Gesbocker <gesbocker@gmail.com>
2022-06-29 17:07:32 -0600

    Feature branch: work in progress

--- fib.js
+++ fib.js
@@ -4,8 +4,10 @@
 // 0, 1, 1, 2, 3, 5
 // output -> 5
 function fib(n, map = {}) {
-  if (n < 2) {
-    map[n] = n === 0 ? 0 : 1;
+  if (n === 0) {
+    map[n] = n;
+  } else if (n < 2) {
+    map[n] = 1;
   }
   if (!(n in map)) {
     map[n] = fib(n - 1, map) + fib(n - 2, map);
@@ -13,4 +15,4 @@ function fib(n, map = {}) {
   return map[n];
 }
 
-console.log(fib(5));
+console.log(fib(49));
